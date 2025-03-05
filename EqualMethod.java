892a2515e7567a927a33dbce7ada08a89aeda710
Sanjeev Jaiswal <sanjeev1822.be21@chitkara.edu.in>
2025-03-05 15:12:26 +0530

    [Sanjeev Jaiswal] ADD: Created method to check equality

---
+++ EqualMethod.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class EqualMethod {
+    public static void main(String[] args) {
+        // String str1 = new String("Hello");
+        // String str2 = new String("Hello");
+
+        String str1 = "Sanjeev";
+        String str2 = "Sanjeev";
+
+        System.out.println(str1 == str2); 
+        System.out.println(str1.equals(str2));
+}
+}
